bcfaacee4ffeb89529bccc9b2c6d62cc56602b95
zhao.zhang <loatheb.zhao@gmail.com>
2021-08-18 21:27:56 +0800

    fix: remove previous dark class names & add story <ConfigBar />

--- src/stories/Switch.stories.tsx
+++ src/stories/Switch.stories.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Switch as SwitchComponent } from '../components';
+import ConfigBar from './Base';
 
 export default {
   title: 'FORM/Switch',
   component: SwitchComponent,
 } as ComponentMeta<typeof SwitchComponent>;
 
-const Template: ComponentStory<typeof SwitchComponent> = (args) => <SwitchComponent {...args} />;
+const Template: ComponentStory<typeof SwitchComponent> = (args) => <><ConfigBar></ConfigBar><SwitchComponent {...args} /></>;
 
 export const Switch = Template.bind({});
