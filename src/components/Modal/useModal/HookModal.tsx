8b829cf649e1c7c0ecf4cddcb7343a56ef9e782d
AmagiDDmxh <amagiddmxh@qq.com>
2021-08-24 22:11:14 +0800

    Add `HookModal`

---
+++ src/components/Modal/useModal/HookModal.tsx
@@ -0,0 +1,65 @@
+import React, { useMemo } from 'react';
+import { ModalProps } from '../Modal';
+import ConfirmDialog from '../ConfirmDialog';
+import { defaultLocale } from '../locale';
+import { useLocale } from '../../Provider/hooks';
+
+export interface HookModalProps {
+  afterClose: () => void;
+  config: ModalProps;
+}
+
+export interface HookModalRef {
+  destroy: () => void;
+  update: (config: ModalProps) => void;
+}
+
+interface ModalLocale {
+  okText: string;
+  cancelText: string;
+  justOkText: string;
+}
+
+const HookModal: React.ForwardRefRenderFunction<HookModalRef, HookModalProps> = (
+  { afterClose, config },
+  ref,
+) => {
+  const [visible, setVisible] = React.useState(true);
+  const [innerConfig, setInnerConfig] = React.useState(config);
+  const { locale } = useLocale()
+  const modalLocale: ModalLocale = useMemo(() => defaultLocale[locale], [locale])
+
+  function close(...args: any[]) {
+    setVisible(false);
+    const triggerCancel = args.some(param => param && param.triggerCancel);
+    if (innerConfig.onCancel && triggerCancel) {
+      innerConfig.onCancel();
+    }
+  }
+
+  React.useImperativeHandle(ref, () => ({
+    destroy: close,
+    update: (newConfig: ModalProps) => {
+      setInnerConfig(originConfig => ({
+        ...originConfig,
+        ...newConfig,
+      }));
+    },
+  }));
+
+  return (
+    <ConfirmDialog
+      {...innerConfig}
+      close={close}
+      visible={visible}
+      afterClose={afterClose}
+      okText={
+        innerConfig.okText ||
+        (innerConfig.okCancel ? modalLocale.okText : modalLocale.justOkText)
+      }
+      cancelText={innerConfig.cancelText || modalLocale.cancelText}
+    />
+  );
+};
+
+export default React.forwardRef(HookModal);
