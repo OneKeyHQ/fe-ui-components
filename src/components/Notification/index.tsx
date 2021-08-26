5454372e07716d226ce27e233a0c97de0010332b
AmagiDDmxh <amagiddmxh@qq.com>
2021-08-26 22:41:12 +0800

    Add static methods

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,3 +1,52 @@
-import Notice from './Notice'
+import { ReactNode } from 'react'
+import OriginNotification, { NotificationProps } from './Notification'
+import { emitter } from './utils'
+import { Events } from './types'
+import { uniqueId } from 'lodash'
 
-export default Notice;
+const createNotificationProps = (props: NotificationProps) => {
+  return {
+    noticeKey: uniqueId(),
+    show: true,
+    ...props,
+  }
+}
+
+type Notifier = (content: ReactNode, props?: NotificationProps) => void
+
+type NotificationType = typeof OriginNotification & {
+  notify: Notifier
+  success: Notifier
+  error: Notifier
+  warn: Notifier
+  processing: Notifier
+  hideAll: () => void
+}
+
+const Notification = OriginNotification as NotificationType
+
+const applyNotification = ({ ...notification }: NotificationProps) => emitter.emit(Events.SHOW, createNotificationProps({ ...notification }))
+
+Notification.notify = (content: ReactNode, props?: NotificationProps) =>
+  applyNotification({ content, type: 'success', ...props })
+
+Notification.success = (content: ReactNode, props?: NotificationProps) =>
+  applyNotification({ content, type: 'success', ...props })
+
+Notification.error = (content: ReactNode, props?: NotificationProps) =>
+  applyNotification({ content, type: 'error', ...props })
+
+Notification.processing = (content: ReactNode, props?: NotificationProps) =>
+  applyNotification({ content, type: 'processing', ...props })
+
+Notification.warn = (content: ReactNode, props?: NotificationProps) =>
+  applyNotification({ content, type: 'warning', ...props })
+
+Notification.hideAll = () => emitter.emit(Events.HIDE_ALL)
+
+
+export * from './types'
+export * from './utils'
+
+export { default as NotificationContainer } from './NotificationContainer'
+export default Notification;
