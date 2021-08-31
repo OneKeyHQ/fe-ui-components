37eb0031350a13f1132c634e54723d3e97fa9090
AmagiDDmxh <amagiddmxh@gmail.com>
2021-08-31 16:03:52 +0800

    Dev/modal (#10)
    * Add basic `Modal` component
    
    * Add confirm component and static methods
    
    * Add runtime locales
    
    * Attach methods to `Modal` and refactor
    
    * Update `Modal` story examples
    
    * Update `Modal` title prop to support react node
    
    * Update `ConfirmDialog` stylings
    
    * Add `HookModal`
    
    * Add `usePatchElement` hook
    
    * Add `useModal` hook
    
    * Expose modal hook
    
    * Add usage of hooked modal
    
    * Add translations to locale file
    
    * Using keys in modal locale instead of values
    
    * Fix icon conflicts and using new get locale method
    
    * Using intl locale method in hook modal
    
    * Update docs and fix known linting errors
    
    * Use non-depupe classnames
    
    * Update typings and docs
    
    * Fix modal confirmation text behavior
    
    * Remove void function condition
    
    * Use `as const` to declare constants

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,59 @@
+import OriginModal, { ModalProps } from './Modal'
+import confirm, {
+  destroyFns,
+  withWarn,
+  withInfo,
+  withSuccess,
+  withError,
+  withConfirm,
+  ModalStaticFunctions,
+} from './confirm';
+import useModal from './useModal'
+
+function modalWarn(props: ModalProps) {
+  return confirm(withWarn(props));
+}
+
+type ModalType = typeof OriginModal &
+  ModalStaticFunctions & {
+    destroyAll: () => void;
+    useModal: typeof useModal;
+    // config: typeof modalGlobalConfig;
+  };
+
+const Modal = OriginModal as ModalType;
+
+Modal.useModal = useModal;
+
+Modal.info = function infoFn(props: ModalProps) {
+  return confirm(withInfo(props));
+};
+
+Modal.success = function successFn(props: ModalProps) {
+  return confirm(withSuccess(props));
+};
+
+Modal.error = function errorFn(props: ModalProps) {
+  return confirm(withError(props));
+};
+
+Modal.warning = modalWarn;
+
+Modal.warn = modalWarn;
+
+Modal.confirm = function confirmFn(props: ModalProps) {
+  return confirm(withConfirm(props));
+};
+
+Modal.destroyAll = function destroyAllFn() {
+  while (destroyFns.length) {
+    const close = destroyFns.pop();
+    if (close) {
+      close();
+    }
+  }
+};
+
+// Modal.config = modalGlobalConfig;
+
+export default Modal;
