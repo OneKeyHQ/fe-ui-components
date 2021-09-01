e9abe72737fec2ef2d9f28258c5a4d11c8c12d79
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-01 14:08:25 +0800

    Lint and format `Modal.tsx` file

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,21 +1,21 @@
-import React, { FC, Fragment, ReactNode } from 'react'
-import cx from 'classnames'
-import { Dialog, Transition } from '@headlessui/react'
-import { useIntl } from 'react-intl'
+import React, { FC, Fragment, ReactNode } from "react";
+import cx from "classnames";
+import { Dialog, Transition } from "@headlessui/react";
+import { useIntl } from "react-intl";
 
-import CheckIcon from '../Icon/react/outline/Check'
-import { modalLocaleKeys } from './locale'
-import UIProvider from '../Provider'
+import CheckIcon from "../Icon/react/outline/Check";
+import { modalLocaleKeys } from "./locale";
+import UIProvider from "../Provider";
 
-type ModalType = 'info' | 'success' | 'error' | 'warn' | 'warning' | 'confirm'
+type ModalType = "info" | "success" | "error" | "warn" | "warning" | "confirm";
 
 export type ModalProps = {
   /** 传入 Modal 组件的 class */
   className?: string;
   /** Modal 标题 */
-  title?: ReactNode
+  title?: ReactNode;
   /** 是否可见 */
-  visible?: boolean
+  visible?: boolean;
   /** 点击确定回调 */
   onOk?: () => void;
   /** 点击模态框右上角叉、取消按钮、Props.maskClosable 值为 true 时的遮罩层或键盘按下 Esc 时的回调 */
@@ -33,36 +33,48 @@ export type ModalProps = {
 };
 
 export const iconColors: Record<ModalType, string> = {
-  'info': 'okd-text-gray-400',
-  'success': 'okd-text-green-400',
-  'error': 'okd-text-red-400',
-  'warn': 'okd-text-yellow-500',
-  'warning': 'okd-text-yellow-500',
-  'confirm': 'okd-text-yellow-500',
-}
+  info: "okd-text-gray-400",
+  success: "okd-text-green-400",
+  error: "okd-text-red-400",
+  warn: "okd-text-yellow-500",
+  warning: "okd-text-yellow-500",
+  confirm: "okd-text-yellow-500",
+};
 
 const iconBgColors: Record<ModalType, string> = {
-  'info': 'okd-bg-gray-100',
-  'success': 'okd-bg-green-100',
-  'error': 'okd-bg-red-100',
-  'warn': 'okd-bg-yellow-100',
-  'warning': 'okd-bg-yellow-100',
-  'confirm': 'okd-bg-yellow-100',
-}
+  info: "okd-bg-gray-100",
+  success: "okd-bg-green-100",
+  error: "okd-bg-red-100",
+  warn: "okd-bg-yellow-100",
+  warning: "okd-bg-yellow-100",
+  confirm: "okd-bg-yellow-100",
+};
 
 const Modal: FC<ModalProps> = (props) => {
-  const { icon, onOk, onCancel, okText, cancelText, visible, title, content, type, children } = props
+  const {
+    icon,
+    onOk,
+    onCancel,
+    okText,
+    cancelText,
+    visible,
+    title,
+    content,
+    type,
+    children,
+  } = props;
 
-  const { formatMessage } = useIntl()
+  const { formatMessage } = useIntl();
 
-  const cancelActionNode = onCancel && <button
-    type="button"
-    className="okd-mt-3 okd-w-full okd-inline-flex okd-justify-center okd-rounded okd-border okd-border-gray-300 okd-shadow-sm okd-px-4 okd-py-2 okd-bg-white okd-text-base okd-font-medium okd-text-gray-700 hover:okd-bg-gray-50 focus:okd-outline-none focus:okd-ring-2 focus:okd-ring-offset-2 focus:okd-ring-brand-500 sm:okd-mt-0 sm:okd-text-sm"
-    onClick={onCancel}
-  >
-    {cancelText || formatMessage({ id: modalLocaleKeys.cancelText })}
-  </button>
-
+  const cancelActionNode = onCancel && (
+    <button
+      type="button"
+      className="okd-mt-3 okd-w-full okd-inline-flex okd-justify-center okd-rounded okd-border okd-border-gray-300 okd-shadow-sm okd-px-4 okd-py-2 okd-bg-white okd-text-base okd-font-medium okd-text-gray-700 hover:okd-bg-gray-50 focus:okd-outline-none focus:okd-ring-2 focus:okd-ring-offset-2 focus:okd-ring-brand-500 sm:okd-mt-0 sm:okd-text-sm"
+      onClick={onCancel}
+    >
+      {cancelText || formatMessage({ id: modalLocaleKeys.cancelText })}
+    </button>
+  );
 
   return (
     <UIProvider>
@@ -87,7 +99,10 @@ const Modal: FC<ModalProps> = (props) => {
             </Transition.Child>
 
             {/* This element is to trick the browser into centering the modal contents. */}
-            <span className="okd-hidden sm:okd-inline-block sm:okd-align-middle sm:okd-h-screen" okd-aria-hidden="true">
+            <span
+              className="okd-hidden sm:okd-inline-block sm:okd-align-middle sm:okd-h-screen"
+              okd-aria-hidden="true"
+            >
               &#8203;
             </span>
             <Transition.Child
@@ -101,19 +116,35 @@ const Modal: FC<ModalProps> = (props) => {
             >
               <div className="okd-inline-block okd-align-bottom okd-bg-white okd-rounded-lg okd-px-4 okd-pt-5 okd-pb-4 okd-text-left okd-overflow-hidden okd-ring-1 okd-ring-black okd-ring-opacity-5 okd-shadow-xl okd-rounded-lg okd-transform okd-transition-all sm:okd-my-8 sm:okd-align-middle sm:okd-max-w-lg sm:okd-w-full sm:okd-p-6">
                 <div>
-                  <div className={cx("okd-mx-auto okd-flex okd-items-center okd-justify-center okd-h-12 okd-w-12 okd-rounded-full", iconBgColors[type ?? 'info'])}>
-                    {icon ?? <CheckIcon className="okd-h-6 okd-w-6 okd-text-green-600" okd-aria-hidden="true" />}
+                  <div
+                    className={cx(
+                      "okd-mx-auto okd-flex okd-items-center okd-justify-center okd-h-12 okd-w-12 okd-rounded-full",
+                      iconBgColors[type ?? "info"]
+                    )}
+                  >
+                    {icon ?? (
+                      <CheckIcon
+                        className="okd-h-6 okd-w-6 okd-text-green-600"
+                        okd-aria-hidden="true"
+                      />
+                    )}
                   </div>
                   <div className="okd-mt-3 okd-text-center sm:okd-mt-5">
-                    <Dialog.Title as="h3" className="okd-text-lg okd-leading-6 okd-font-medium okd-text-gray-900">
+                    <Dialog.Title
+                      as="h3"
+                      className="okd-text-lg okd-leading-6 okd-font-medium okd-text-gray-900"
+                    >
                       {title}
                     </Dialog.Title>
-                    <div className="okd-mt-2">
-                      {content ?? children}
-                    </div>
+                    <div className="okd-mt-2">{content ?? children}</div>
                   </div>
                 </div>
-                <div className={cx("okd-mt-5 sm:okd-mt-6 sm:okd-grid sm:okd-gap-3 sm:okd-grid-flow-row-dense", cancelActionNode && 'sm:okd-grid-cols-2')}>
+                <div
+                  className={cx(
+                    "okd-mt-5 sm:okd-mt-6 sm:okd-grid sm:okd-gap-3 sm:okd-grid-flow-row-dense",
+                    cancelActionNode && "sm:okd-grid-cols-2"
+                  )}
+                >
                   {cancelActionNode}
                   <button
                     className="okd-w-full okd-inline-flex okd-justify-center okd-rounded okd-border okd-border-transparent okd-shadow-sm okd-px-4 okd-py-2 okd-bg-brand-500 okd-text-base okd-font-medium okd-text-white hover:okd-bg-brand-600 focus:okd-outline-none focus:okd-ring-2 focus:okd-ring-offset-2 focus:okd-ring-brand-500 sm:okd-text-sm"
@@ -128,7 +159,7 @@ const Modal: FC<ModalProps> = (props) => {
         </Dialog>
       </Transition.Root>
     </UIProvider>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
