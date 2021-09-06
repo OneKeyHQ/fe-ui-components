e648087c482eb776b525f04dcd2ac7e816334da4
zhao.zhang <loatheb.zhao@gmail.com>
2021-09-06 19:33:28 +0800

    feat: add i18n for connector & fix button props type

--- src/components/WalletSelector/index.tsx
+++ src/components/WalletSelector/index.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useCallback } from "react";
 import { AbstractConnectorArguments } from "@web3-react/types";
 import { AbstractConnector } from "@web3-react/abstract-connector";
-import { UnsupportedChainIdError, useWeb3React } from "../web3/core";
 
 import { buildMetaMaskConnector, buildOneKeyConnector } from "./connectors";
 
+import { UnsupportedChainIdError, useWeb3React } from "../web3/core";
+import { FormattedMessage, useIntl } from "../Intl";
+import Button from "../Button";
 import Icon from "../Icon";
 import Modal from "../Modal";
 
@@ -26,6 +28,7 @@ const WalletSelector: FC<WalletSelectorProps> = ({
   onClose,
   connectorConfig,
 }) => {
+  const intl = useIntl();
   const { active, account, connector, activate, error } = useWeb3React();
 
   const wallets = [
@@ -75,33 +78,44 @@ const WalletSelector: FC<WalletSelectorProps> = ({
       onClose={onClose}
       className="okd-w-full sm:okd-max-w-md"
     >
-      <Modal.Header title="Connect Wallet" onClose={onClose} />
+      <Modal.Header
+        title={intl.formatMessage({ id: "ui-components__connect_a_wallet" })}
+        onClose={onClose}
+      />
       <Modal.Body>
         <div className="max-w-full okd-flex okd-flex-col okd-space-y-3">
           {wallets.map((wallet) => {
             if (!wallet.hasLoad) {
               return (
-                <a
+                <Button
+                  block
+                  size="xl"
+                  type="link"
                   href={wallet.downloadLink}
-                  target="_blank"
-                  rel="noreferrer"
                   key={wallet.name}
-                  className="okd-flex okd-items-center okd-justify-between okd-p-4 okd-font-medium okd-text-gray-900 okd-rounded okd-border okd-border-gray-200 focus:okd-ring-2 focus:okd-ring-offset-2 focus:okd-ring-brand-500 focus:okd-ring-offset-white focus:okd-outline-none hover:okd-bg-gray-50"
+                  target="_blank"
+                  className="okd-flex okd-items-center okd-justify-between okd-border okd-border-gray-200"
                 >
-                  <span>download {wallet.name}</span>
+                  <span className="okd-text-gray-900">
+                    <FormattedMessage id="ui-components__connect__option_item__install_onekey" />
+                  </span>
                   <Icon className="okd-ml-4" name={wallet.logo} />
-                </a>
+                </Button>
               );
             }
             return (
-              <button
+              <Button
+                block
+                size="xl"
                 onClick={() => tryActivation(wallet.connector, wallet)}
                 key={wallet.name}
-                className="okd-flex okd-items-center okd-justify-between okd-p-4 okd-font-medium okd-text-gray-900 okd-rounded okd-border okd-border-gray-200 focus:okd-ring-2 focus:okd-ring-offset-2 focus:okd-ring-brand-500 focus:okd-ring-offset-white focus:okd-outline-none hover:okd-bg-gray-50"
+                className="okd-flex okd-items-center okd-justify-between okd-border okd-border-gray-200"
               >
-                <span>{wallet.name}</span>
+                <span className="okd-text-gray-900">
+                  <FormattedMessage id="ui-components__connect__option_item__install_onekey" />
+                </span>
                 <Icon className="okd-ml-4" name={wallet.logo} />
-              </button>
+              </Button>
             );
           })}
         </div>
