055ac813a87ee4288dbc574d8efaa02aeafe8a48
Franco <chenhuifan.gg@gmail.com>
2021-09-06 22:21:54 +0800

    Revert "custom logo's color (#37)" (#38)
    This reverts commit f8ce73471cb770a7cca2ee097540d1ee1a28f35e.
    
    Co-authored-by: zhao.zhang <loatheb.zhao@gmail.com>

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -107,7 +107,7 @@ const Sidebar: FC<SidebarProps> = ({
         {/* Brand */}
         <div className="okd-flex okd-items-center okd-flex-shrink-0 okd-pl-4">
           <Icon
-            className="okd-w-7 okd-h-7 okd-text-brand-500"
+            className="okd-w-7 okd-h-7"
             name="BrandLogoIllus"
             size={28}
             aria-label="The Brand Logo – OneKey"
@@ -189,7 +189,7 @@ const Sidebar: FC<SidebarProps> = ({
               {/* Brand */}
               <div className="okd-flex okd-items-center okd-flex-shrink-0 okd-pl-[22px]">
                 <Icon
-                  className="okd-w-7 okd-h-7 okd-text-brand-500"
+                  className="okd-w-7 okd-h-7"
                   name="BrandLogoIllus"
                   size={28}
                   aria-label="The Brand Logo – OneKey"
@@ -388,7 +388,7 @@ const Sidebar: FC<SidebarProps> = ({
             onClick={() => window.location.reload()}
           >
             <Icon
-              className="okd-w-7 okd-h-7 okd-text-brand-500"
+              className="okd-w-7 okd-h-7"
               name="BrandLogoIllus"
               size={28}
               aria-label="The Brand Logo – OneKey"
