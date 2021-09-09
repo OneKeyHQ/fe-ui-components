43a43de5b3dbe9d39502f7c558b5980161a4bcae
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-09 20:30:46 +0800

    Add hint prop to show level up hint on `Progress`

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import type { FC } from "react";
+import { useIntl } from "react-intl";
+
 import { useProportions } from "./portion";
 
 export type ProgressProps = {
@@ -11,31 +13,50 @@ export type ProgressProps = {
    * 最大值
    */
   max?: number;
+  hint?: boolean;
+  customHint?: ReactNode;
 };
 
 const defaultProps = {
   value: 0,
   max: 100,
+  hint: false,
 } as const;
 
 /**
  * Progress 是一个进度条组件，用于展示事件或与之相关的任务进展。
  */
-const Progress: FC<ProgressProps> = ({ value, max = 100 }) => {
+const Progress: FC<ProgressProps> = ({ value, max, hint, customHint }) => {
   const portionValue = useProportions(value, max);
+  const { formatMessage } = useIntl();
+
+  const levelUpText = formatMessage(
+    { id: "ui-components__progress__how_much_to_level_up" },
+    { value }
+  );
+
+  const hintNode = (!!customHint || hint) && (
+    <div className="okd-flex okd-justify-between okd-text-black okd-text-xs okd-font-normal">
+      <p>{customHint || levelUpText}</p>
+      <p>{max}</p>
+    </div>
+  );
 
   return (
-    <div className="okd-relative okd-bg-green-200 okd-w-full okd-h-[6px] okd-rounded">
-      <div
-        className="okd-absolute okd-inset-0 okd-h-full okd-transition-all okd-duration-100 okd-ease-in okd-rounded okd-bg-green-500"
-        style={{ width: `${portionValue}%` }}
-        title={`${portionValue}%`}
-      />
-      <progress
-        value={value}
-        max={max}
-        className="okd-fixed okd--top-[1000px] okd-opacity-0 okd-invisible okd-pointer-events-none"
-      />
+    <div className="okd-space-y-2 okd-w-full">
+      <div className="okd-relative okd-bg-green-200 okd-h-[6px] okd-rounded">
+        <div
+          className="okd-absolute okd-inset-0 okd-h-full okd-transition-all okd-duration-100 okd-ease-in okd-rounded okd-bg-green-500"
+          style={{ width: `${portionValue}%` }}
+          title={`${portionValue}%`}
+        />
+        <progress
+          value={value}
+          max={max}
+          className="okd-fixed okd--top-[1000px] okd-opacity-0 okd-invisible okd-pointer-events-none"
+        />
+      </div>
+      {hintNode}
     </div>
   );
 };
