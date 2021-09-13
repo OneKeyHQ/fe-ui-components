8f18c7a7d599cff26efe8ba2ada7e8ce02182e86
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-13 15:35:27 +0800

    Add `QuestModal` empty case story

--- src/stories/QuestModal.stories.tsx
+++ src/stories/QuestModal.stories.tsx
@@ -179,3 +179,157 @@ export const Default = () => {
     </>
   );
 };
+
+export const EmptyCase = () => {
+  const address = "0x8260d4AaC59452c54f350047dCb67827C5cE715a";
+
+  const [isQuestModalVisible, setIsQuestModalVisible] = useState(false);
+  const handleCloseQuestModal = useCallback(
+    () => setIsQuestModalVisible(false),
+    [setIsQuestModalVisible]
+  );
+
+  return (
+    <>
+      <ConfigBar />
+      <Button onClick={() => setIsQuestModalVisible(true)}>
+        Open Quest Modal
+      </Button>
+
+      <Modal
+        className="okd-w-auto"
+        onClose={handleCloseQuestModal}
+        visible={isQuestModalVisible}
+      >
+        <Modal.Header
+          title="Quests"
+          actions={
+            <div className="okd-flex okd-space-x-6">
+              <div className="okd-flex okd-w-5 okd-h-5 okd-items-center okd-justify-center">
+                <Button
+                  circular
+                  type="plain"
+                  leadingIcon="QuestionMarkCircleSolid"
+                />
+              </div>
+              <div className="okd-flex okd-w-5 okd-h-5 okd-items-center okd-justify-center">
+                <Button circular type="plain" leadingIcon="ClockSolid" />
+              </div>
+            </div>
+          }
+          onClose={handleCloseQuestModal}
+        />
+        <div className="okd-space-y-6 okd-px-4 sm:okd-px-6 okd-py-6">
+          <div className="okd-space-y-2">
+            <div className="okd-flex okd-items-center okd-justify-between">
+              <div className="okd-flex-shrink-0 okd-flex okd-items-center okd-space-x-2 okd-h-8">
+                <Avatar address={address} />
+                <Address
+                  className="okd-text-black okd-text-sm okd-font-normal"
+                  address={address}
+                  short
+                />
+              </div>
+              <LevelBadge level={1} />
+            </div>
+            <Progress value={300} max={600} hint />
+          </div>
+
+          <div className="okd-space-y-4">
+            <h5 className="okd-text-gray-700 okd-text-sm okd-font-medium okd-leading-5">
+              NFT Addition
+            </h5>
+
+            <NFTStackedCard
+              className="okd-h-[160px]"
+              title={
+                <div className="okd-flex okd-items-end">
+                  <p className="okd-text-gray-900 okd-text-4xl okd-font-semibold">
+                    0
+                  </p>
+                  <p className="okd-text-gray-700 okd-text-sm okd-font-medium okd-ml-2 okd-leading-7">
+                    XP / DAY
+                  </p>
+                </div>
+              }
+              subTitle={
+                <p className="okd-text-gray-500 okd-text-xs okd-font-normal">
+                  Cumulative: 0 XP
+                </p>
+              }
+              action={<Button size="xs">View My NFTs</Button>}
+            />
+          </div>
+
+          <div className="okd-space-y-4">
+            <h5 className="okd-text-gray-700 okd-text-sm okd-font-medium okd-leading-5">
+              Daily Quests
+            </h5>
+
+            <div className="okd-space-y-2">
+              <Card>
+                <div className="okd-flex okd-justify-between okd-items-center">
+                  <div>
+                    <h5 className="okd-text-gray-700 okd-text-sm okd-font-medium okd-leading-5">
+                      Daily check in
+                    </h5>
+                    <h5 className="okd-text-gray-400 okd-text-sm okd-font-normal okd-leading-5">
+                      Rewards: +2 XP
+                    </h5>
+                  </div>
+                  <Button type="primary">Check in</Button>
+                </div>
+              </Card>
+
+              <Card>
+                <div className="okd-flex okd-justify-between okd-items-center">
+                  <div>
+                    <h5 className="okd-text-gray-700 okd-text-sm okd-font-medium okd-leading-5">
+                      Daily first swap
+                    </h5>
+                    <div className="okd-inline-flex okd-items-center okd-space-x-1">
+                      <p className="okd-text-gray-400 okd-text-sm okd-font-normal okd-leading-5">
+                        Rewards: +xx XP
+                      </p>
+                      <Tooltip content="Swap rewards help content">
+                        <Icon
+                          name="QuestionMarkCircleSolid"
+                          className="okd-text-gray-300"
+                        />
+                      </Tooltip>
+                    </div>
+                  </div>
+                  <Button type="primary" disabled>
+                    Swaped
+                  </Button>
+                </div>
+              </Card>
+
+              <Card>
+                <div className="okd-flex okd-justify-between okd-items-center">
+                  <div>
+                    <h5 className="okd-text-gray-700 okd-text-sm okd-font-medium okd-leading-5">
+                      Swap Tokens
+                    </h5>
+                    <div className="okd-inline-flex okd-items-center okd-space-x-1">
+                      <p className="okd-text-gray-400 okd-text-sm okd-font-normal okd-leading-5">
+                        Rewards: +0.05% swap value{" "}
+                      </p>
+                      <Tooltip content="Swap rewards help content">
+                        <Icon
+                          name="QuestionMarkCircleSolid"
+                          className="okd-text-gray-300"
+                        />
+                      </Tooltip>
+                    </div>
+                  </div>
+                  <Button type="primary">Go to Swap</Button>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
