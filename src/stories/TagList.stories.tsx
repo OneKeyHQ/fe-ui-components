a2f7eda6557661c2117bdacd8d335a78eaefb1f4
zhao.zhang <loatheb.zhao@gmail.com>
2021-09-14 18:19:35 +0800

    fix: <Tag /> <TagList /> remove & active issue

--- src/stories/TagList.stories.tsx
+++ src/stories/TagList.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { TagList as TagListComponent } from "../components";
 
@@ -7,83 +7,183 @@ export default {
   component: TagListComponent,
 } as ComponentMeta<typeof TagListComponent>;
 
-const Template: ComponentStory<typeof TagListComponent> = (args) => (
-  <TagListComponent {...args}>
-  </TagListComponent>
+export const TagList: ComponentStory<typeof TagListComponent> = () => (
+  <TagListComponent
+    clickable
+    tags={[
+      {
+        token: {
+          chain: 'eth'
+        },
+        children: 1,
+      },
+      {
+        token: {
+          chain: 'bsc'
+        },
+        children: 2,
+      },
+      {
+        token: {
+          chain: 'bsc'
+        },
+        children: 'asdfasdfasdfasdfasdfasfs',
+      }
+    ]}
+  />
 );
 
-export const TagList = Template.bind({});
-TagList.args = {
-  tags: [
-    {
-      token: {
-        chain: 'eth'
-      },
-      children: 1,
-    },
-    {
-      token: {
-        chain: 'bsc'
-      },
-      children: 2,
-    },
-    {
-      token: {
-        chain: 'bsc'
-      },
-      children: 'asdfasdfasdfasdfasdfasfs',
-    }
-  ]
-};
 
-export const TagListControl = Template.bind({});
-TagListControl.args = {
-  multi: true,
-  onChange(active) {
-    console.log('current select is:', active);
-  },
-  tags: [
-    {
-      token: {
-        chain: 'eth'
+export const TagListControl: ComponentStory<typeof TagListComponent> = (args) => {
+  const [active, setActive] = useState(0);
+  return (
+    <>
+      <p>current select is index {active + 1}</p>
+      <TagListComponent<typeof active>
+        clickable
+        value={active}
+        onChange={setActive}
+        tags={
+          [
+            {
+              token: {
+                chain: 'eth'
+              },
+              children: 1,
+            },
+            {
+              token: {
+                chain: 'bsc'
+              },
+              children: 2,
+            },
+            {
+              token: {
+                chain: 'bsc'
+              },
+              children: 'asdfasdfasdfasdfasdfasfs',
+            }
+          ]
+        }
+      />
+    </>
+  );
+}
+
+export const TagListMultipleControl: ComponentStory<typeof TagListComponent> = (args) => {
+  const [active, setActive] = useState([0, 1]);
+  return (
+    <>
+      <p>current select is index {active.map(i => i + 1)}</p>
+      <TagListComponent<typeof active>
+        multi
+        clickable
+        value={active}
+        onChange={setActive}
+        tags={
+          [
+            {
+              token: {
+                chain: 'eth'
+              },
+              children: 1,
+            },
+            {
+              token: {
+                chain: 'bsc'
+              },
+              children: 2,
+            },
+            {
+              token: {
+                chain: 'bsc'
+              },
+              children: 'asdfasdfasdfasdfasdfasfs',
+            }
+          ]
+        }
+      />
+    </>
+  );
+}
+
+export const TagListMultipleRemoveControl: ComponentStory<typeof TagListComponent> = () => {
+  const [tags, setTags] = useState(
+    [
+      {
+        token: {
+          chain: 'eth'
+        },
+        children: 1,
       },
-      children: 'this is first one',
-    },
-    {
-      token: {
-        chain: 'bsc'
+      {
+        token: {
+          chain: 'bsc'
+        },
+        children: 2,
       },
-      children: 'this is second one',
-    },
-    {
-      token: {
-        chain: 'bsc'
+      {
+        token: {
+          chain: 'bsc'
+        },
+        children: 'asdfasdfasdfasdfasdfasfs',
+      }
+    ]
+  );
+
+  return (
+    <>
+      <TagListComponent
+        multi
+        onRemove={(i, item) => {
+          setTags(tags => tags.filter((item, index) => index !== i))
+        }}
+        tags={tags}
+      />
+    </>
+  );
+}
+
+
+export const TagListMultipleClickRemoveControl: ComponentStory<typeof TagListComponent> = () => {
+  const [activeIndexs, setActiveIndex] = useState([]);
+  const [tags, setTags] = useState(
+    [
+      {
+        token: {
+          chain: 'eth'
+        },
+        children: 1,
       },
-      children: 'this is second one',
-    },
-    {
-      token: {
-        chain: 'bsc'
+      {
+        token: {
+          chain: 'bsc'
+        },
+        children: 2,
       },
-      children: 'this is second one',
-    },
-    {
-      token: {
-        chain: 'bsc'
-      },
-      children: 'this is second one',
-    },
-    {
-      token: {
-        chain: 'bsc'
-      },
-      children: 'this is second one',
-    },
-    {
-      token: {
-        chain: 'bsc'
-      },
-      children: 'asdfasdfasdfasdfasdfasfs',
-    }
-  ]
-};
+      {
+        token: {
+          chain: 'bsc'
+        },
+        children: 'asdfasdfasdfasdfasdfasfs',
+      }
+    ]
+  );
+
+  return (
+    <>
+      <p>current select is index {activeIndexs.map(i => i + 1)}</p>
+      <TagListComponent<typeof activeIndexs>
+        multi
+        clickable
+        value={activeIndexs}
+        onChange={setActiveIndex}
+        onRemove={(i) => {
+          setTags(tags => tags.filter((_, index) => index !== i))
+          setActiveIndex(prev => prev.filter((item) => item !== i))
+        }}
+        tags={tags}
+      />
+    </>
+  );
+}
