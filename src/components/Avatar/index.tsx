eff0d76e80c4bb5a7e94a5b9caa72d302b7bb84d
zhao.zhang <loatheb.zhao@gmail.com>
2021-09-15 18:32:36 +0800

    fix: <Jazzicon /> not render issue

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import cx from "classnames";
-import JazzIcon from "react-jazzicon";
+import jazzicon from 'jazzicon';
 
 type AvatarProps = {
   /**
@@ -18,9 +18,27 @@ const defaultProps = {
 } as const;
 
 const Avatar: FC<AvatarProps> = ({ address, size }) => {
+  const ref = useRef<HTMLDivElement>();
+  useEffect(() => {
+    if (address && ref.current) {
+      ref.current.innerHTML = '';
+      ref.current.appendChild(jazzicon(size, parseInt(address.slice(2, 10), 16)));
+    }
+  }, [address, size]);
+
   return (
     <div className={cx("okd-overflow-hidden okd-inline-block")}>
-      <JazzIcon diameter={size} seed={address} />
+      <div
+        ref={ref}
+        className={cx({
+          "okd-w-5 okd-h-5": (size === 20),
+          "okd-w-6 okd-h-6": (size === 24),
+          "okd-w-8 okd-h-8": (size === 32),
+          "okd-w-10 okd-h-10": (size === 40),
+          "okd-w-12 okd-h-12": (size === 48),
+          "okd-w-14 okd-h-14": (size === 56),
+        })}
+      />
     </div>
   );
 };
--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -16,3 +16,7 @@ export const Avatar = Template.bind({});
 Avatar.args = {
   address: "0x0000000000000000000000000000000000000000",
 };
+
+export const Avatar2: ComponentStory<typeof AvatarComponent> = () => (
+  <AvatarComponent address="0x1f73f8C993bf0e2b5a860Ffd93E135103208a707" />
+);
