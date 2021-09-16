09e6dec906fdfefc4026df4daf58222dc14e06ab
zhao.zhang <loatheb.zhao@gmail.com>
2021-09-16 10:19:48 +0800

    fix: jazzicon next.js ssg issue. (#95)
    * fix: jazzicon ssg window issue
    
    * release: upgrade to v0.0.26

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC } from "react";
 import cx from "classnames";
-import jazzicon from 'jazzicon';
+import JazzIcon from "react-jazzicon";
 
 type AvatarProps = {
   /**
@@ -18,27 +18,10 @@ const defaultProps = {
 } as const;
 
 const Avatar: FC<AvatarProps> = ({ address, size }) => {
-  const ref = useRef<HTMLDivElement>();
-  useEffect(() => {
-    if (address && ref.current) {
-      ref.current.innerHTML = '';
-      ref.current.appendChild(jazzicon(size, parseInt(address.slice(2, 10), 16)));
-    }
-  }, [address, size]);
-
+  const seed = parseInt(address.slice(2, 10), 16);
   return (
     <div className={cx("okd-overflow-hidden okd-inline-block")}>
-      <div
-        ref={ref}
-        className={cx({
-          "okd-w-5 okd-h-5": (size === 20),
-          "okd-w-6 okd-h-6": (size === 24),
-          "okd-w-8 okd-h-8": (size === 32),
-          "okd-w-10 okd-h-10": (size === 40),
-          "okd-w-12 okd-h-12": (size === 48),
-          "okd-w-14 okd-h-14": (size === 56),
-        })}
-      />
+      <JazzIcon diameter={size} seed={seed} />
     </div>
   );
 };
