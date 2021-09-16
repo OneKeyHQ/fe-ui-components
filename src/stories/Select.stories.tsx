bd975243134f470e449cce0ae8375a7b66aec181
Franco <chenhuifan.gg@gmail.com>
2021-09-16 10:35:59 +0800

    select styles optimization & update stories (#94)

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -3,19 +3,16 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Select as SelectComponent, Icon } from "../components";
 import ConfigBar from "./Base";
+import { Token } from "../components";
 
 export default {
   title: "UI/Select",
   component: SelectComponent,
 } as ComponentMeta<typeof SelectComponent>;
 
-const Wrapper = ({children}) => {
-  return (
-    <div className="okd-h-96">
-      {children}
-    </div>
-  )
-}
+const Wrapper = ({ children }) => {
+  return <div className="okd-h-96">{children}</div>;
+};
 
 const options = [
   { value: "chocolate", label: "Chocolate" },
@@ -27,9 +24,7 @@ export const Default: ComponentStory<typeof SelectComponent> = () => (
   <>
     <ConfigBar />
     <Wrapper>
-      <SelectComponent
-        options={options}
-      />
+      <SelectComponent options={options} />
     </Wrapper>
   </>
 );
@@ -38,10 +33,7 @@ export const Disable: ComponentStory<typeof SelectComponent> = () => (
   <>
     <ConfigBar />
     <Wrapper>
-      <SelectComponent
-        isDisabled
-        options={options}
-      />
+      <SelectComponent isDisabled options={options} />
     </Wrapper>
   </>
 );
@@ -50,23 +42,16 @@ export const MultipleSelect: ComponentStory<typeof SelectComponent> = () => (
   <>
     <ConfigBar />
     <Wrapper>
-      <SelectComponent
-        multi
-        isClearable
-        options={options}
-      />
+      <SelectComponent multi isClearable options={options} />
     </Wrapper>
   </>
 );
-
 
 export const Loading: ComponentStory<typeof SelectComponent> = () => (
   <>
     <ConfigBar />
     <Wrapper>
-      <SelectComponent
-        isLoading
-      />
+      <SelectComponent isLoading />
     </Wrapper>
   </>
 );
@@ -79,20 +64,20 @@ export const Group: ComponentStory<typeof SelectComponent> = () => (
         multi
         options={[
           {
-            label: 'first group',
+            label: "first group",
             options: [
               { value: "chocolate", label: "Chocolate" },
               { value: "strawberry", label: "Strawberry" },
               { value: "vanilla", label: "Vanilla" },
-            ]
+            ],
           },
           {
-            label: 'second group',
+            label: "second group",
             options: [
               { value: "chocolate", label: "Chocolate" },
               { value: "strawberry", label: "Strawberry" },
               { value: "vanilla", label: "Vanilla" },
-            ]
+            ],
           },
         ]}
       />
@@ -106,23 +91,28 @@ export const PrefixIcon: ComponentStory<typeof SelectComponent> = () => (
     <Wrapper>
       <SelectComponent
         multi
-        prefix={<Icon name="SearchOutline" className={"okd-w-5 okd-h-5 okd-ml-3"} />}
+        prefix={
+          <Icon
+            name="SearchOutline"
+            className={"okd-w-5 okd-h-5 okd-ml-3 okd-text-gray-400 okd--mr-1"}
+          />
+        }
         options={[
           {
-            label: 'first group',
+            label: "first group",
             options: [
               { value: "chocolate", label: "Chocolate" },
               { value: "strawberry", label: "Strawberry" },
               { value: "vanilla", label: "Vanilla" },
-            ]
+            ],
           },
           {
-            label: 'second group',
+            label: "second group",
             options: [
               { value: "chocolate", label: "Chocolate" },
               { value: "strawberry", label: "Strawberry" },
               { value: "vanilla", label: "Vanilla" },
-            ]
+            ],
           },
         ]}
       />
@@ -130,33 +120,55 @@ export const PrefixIcon: ComponentStory<typeof SelectComponent> = () => (
   </>
 );
 
-export const CustomRenderOption: ComponentStory<typeof SelectComponent> = () => (
+export const CustomRenderOption: ComponentStory<
+  typeof SelectComponent
+> = () => (
   <>
     <ConfigBar />
     <Wrapper>
       <SelectComponent
         multi
-        prefix={<Icon name="SearchOutline" className={"okd-w-5 okd-h-5 okd-ml-3"} />}
+        prefix={
+          <Icon
+            name="SearchOutline"
+            className={"okd-w-5 okd-h-5 okd-ml-3 okd-text-gray-400 okd--mr-1"}
+          />
+        }
         options={[
           {
-            label: 'first group',
+            label: "first group",
             options: [
               { value: "chocolate", label: "Chocolate" },
               { value: "strawberry", label: "Strawberry" },
               { value: "vanilla", label: "Vanilla" },
-            ]
+            ],
           },
           {
-            label: 'second group',
+            label: "second group",
             options: [
               { value: "chocolate", label: "Chocolate" },
               { value: "strawberry", label: "Strawberry" },
               { value: "vanilla", label: "Vanilla" },
-            ]
+            ],
           },
         ]}
         renderOption={(data, props) => {
-          return (<div {...props.innerProps}>{data.label}</div>)
+          return (
+            <div
+              className="okd-py-2 okd-px-3 okd-rounded okd-text-gray-900 hover:okd-bg-gray-50 okd-cursor-default focus:okd-bg-gray-100"
+              {...props.innerProps}
+            >
+              <div className="okd-flex okd-items-center">
+                {/* if token / if leadingContent / something else */}
+                <Token chain="bsc" className="okd-mr-3" />
+                <div>
+                  {data.label}
+                  {/* if description */}
+                  <div className="okd-text-gray-500">Description</div>
+                </div>
+              </div>
+            </div>
+          );
         }}
       />
     </Wrapper>
