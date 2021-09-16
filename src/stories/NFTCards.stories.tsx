97fda76b823041d21aa0bd2925c17a1ed4cf00f3
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-16 21:28:21 +0800

    Add amount show case

--- src/stories/NFTCards.stories.tsx
+++ src/stories/NFTCards.stories.tsx
@@ -12,6 +12,10 @@ export const Default = () => {
     <>
       <ConfigBar />
       <div className="okd-grid sm:okd-grid-cols-2 md:okd-grid-cols-3 lg:okd-grid-cols-4 okd-gap-5">
+        <OneKeyNFT amount={5} />
+        <OneKeyNFT amount={4} />
+        <OneKeyNFT amount={3} />
+        <OneKeyNFT amount={2} />
         <OneKeyNFT />
         <OneKeyNFT />
         <OneKeyNFT />
