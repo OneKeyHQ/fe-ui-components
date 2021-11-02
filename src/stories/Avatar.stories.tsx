b91324a7872879bd5d6dc553e45765e711ee75af
AmagiDDmxh <amagiddmxh@qq.com>
2021-11-02 13:21:54 +0800

    Add avatar error show case

--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -20,3 +20,21 @@ Avatar.args = {
 export const Avatar2: ComponentStory<typeof AvatarComponent> = () => (
   <AvatarComponent address="0x1f73f8C993bf0e2b5a860Ffd93E135103208a707" />
 );
+
+export const RemoteErrorFallback: ComponentStory<
+  typeof AvatarComponent
+> = () => (
+  <AvatarComponent
+    address="0x0000000000000000000000000000000000000000"
+    isRemote
+  />
+);
+
+export const RemoteErrorFallback2: ComponentStory<
+  typeof AvatarComponent
+> = () => (
+  <AvatarComponent
+    logoUrl="Wrong logo url"
+    address="0x0000000000000000000000000000000000000000"
+  />
+);
